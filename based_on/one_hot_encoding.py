2dacee7bdd7f124f3730f987f97a602cdcd531c8
Igor Strozzi <istrozzi@matematica.ufrj.br>
2022-05-22 01:52:45 -0300

    one-hot-encoding

--- based_on/one_hot_encoding.py
+++ based_on/one_hot_encoding.py
@@ -1,24 +1,23 @@
 # -*- coding: UTF-8 -*-
+
+# %%
+
 import numpy as np
-import captcha_setting
+import captcha_settings as cs
+
+# %%
+
+char_to_pos = {c: i for i, c in enumerate(cs.ALL_CHAR_SET)}
+pos_to_char = {i: c for i, c in enumerate(cs.ALL_CHAR_SET)}
+
+# %%
 
 
 def encode(text):
-    vector = np.zeros(captcha_setting.ALL_CHAR_SET_LEN * captcha_setting.MAX_CAPTCHA, dtype=float)
-    def char2pos(c):
-        if c =='_':
-            k = 62
-            return k
-        k = ord(c)-48
-        if k > 9:
-            k = ord(c) - 65 + 10
-            if k > 35:
-                k = ord(c) - 97 + 26 + 10
-                if k > 61:
-                    raise ValueError('error')
-        return k
+    vector = np.zeros(cs.ALL_CHAR_SET_LEN * cs.MAX_CAPTCHA, dtype=float)
+
     for i, c in enumerate(text):
-        idx = i * captcha_setting.ALL_CHAR_SET_LEN + char2pos(c)
+        idx = i * cs.ALL_CHAR_SET_LEN + char_to_pos[c]
         vector[idx] = 1.0
     return vector
 
@@ -26,21 +25,15 @@ def decode(vec):
     char_pos = vec.nonzero()[0]
     text=[]
     for i, c in enumerate(char_pos):
-        char_at_pos = i #c/63
-        char_idx = c % captcha_setting.ALL_CHAR_SET_LEN
-        if char_idx < 10:
-            char_code = char_idx + ord('0')
-        elif char_idx <36:
-            char_code = char_idx - 10 + ord('A')
-        elif char_idx < 62:
-            char_code = char_idx - 36 + ord('a')
-        elif char_idx == 62:
-            char_code = ord('_')
-        else:
-            raise ValueError('error')
-        text.append(chr(char_code))
+        code = c % cs.ALL_CHAR_SET_LEN
+        text.append(pos_to_char[code])
     return "".join(text)
 
+# %%
+
 if __name__ == '__main__':
-    e = encode("BK7H")
+    e = encode("@k7%")
+    print(len(e))
+    print(e)
     print(decode(e))
+# %%
---
+++ dataset_gen/captcha_settings.py
@@ -0,0 +1,38 @@
+
+# %%
+
+import os
+
+# string.digits + string.ascii_uppercase
+NUMBER = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+UPPER_ALPHABET = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+LOWER_ALPHABET = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+SPECIAL_CHARACTERS = list('+@#%=&<>?}{')
+
+# %%
+
+
+ALL_CHAR_SET = NUMBER + UPPER_ALPHABET + LOWER_ALPHABET + SPECIAL_CHARACTERS
+# ALL_CHAR_SET = NUMBER + UPPER_ALPHABET + LOWER_ALPHABET 
+ALL_CHAR_SET_LEN = len(ALL_CHAR_SET)
+# %%
+
+MAX_CAPTCHA = 4
+
+
+IMAGE_HEIGHT = 34
+IMAGE_WIDTH = 81
+
+TRAIN_DATASET_PATH = 'dataset' + os.path.sep + 'train'
+TEST_DATASET_PATH = 'dataset' + os.path.sep + 'test'
+PREDICT_DATASET_PATH = 'dataset' + os.path.sep + 'predict'
+RAW_DATASET_PATH = ".." + os.path.sep + 'dataset' + os.path.sep + 'raw'
+
+# %%
+NUM_CROSSES = [2, 3]
+NUM_LINES = [3, 5]
+
+FONT_SIZE = 23
+FONT = "Arial.ttf"
+
+BG_COLOR = (255, 255, 126)
